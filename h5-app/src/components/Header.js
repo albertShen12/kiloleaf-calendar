c11a321cfe970b77caab5a08a29f914cdf9fb708
albert.shen <albert.shen@foxmail.com>
2019-12-30 00:20:31 +0800

    homepage header

--- h5-app/src/components/Header.js
+++ h5-app/src/components/Header.js
@@ -1,5 +1,13 @@
 import React, {useState}  from "react";
-import {Button,Progress} from "antd";
+import {Icon,Menu,Dropdown} from "antd";
+
+const UserDropdown=()=>(
+  <Menu>
+    <Menu.Item>个人中心</Menu.Item>
+    <Menu.Item>我的团队</Menu.Item>
+    <Menu.Item>退出登录</Menu.Item>
+  </Menu>
+);
 
 const Header = props => {
   const [percent,setPercent]=useState(70);
@@ -9,10 +17,22 @@ const Header = props => {
       setPercent(70);
     },2000);
   };
+
   return (
     <header className="qy-header">
-      <h2>{props.text}<Button type="primary" onClick={handleClick}>Click me!</Button></h2>
-      <div> <Progress percent={percent} status="active" /></div>
+      <div className="header-left">
+        <Icon type="home" />
+        <span>千叶日历</span>
+      </div>
+      <div className="header-right">
+         <Dropdown overlay={UserDropdown}>
+
+            <div className="header-user">
+               <img src={process.env.PUBLIC_URL+'/lu.jpg'}/>
+               <Icon type="down" style={{fontSize:"20px"}} />
+               </div>
+         </Dropdown>
+      </div>
     </header>
   );
 };
